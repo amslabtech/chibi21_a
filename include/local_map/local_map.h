94f9f5bdb66aca0aad19dee19dd9eaf946a4301b
morisitakanta <morisitakanta@gmail.com>
2021-04-05 16:03:52 +0900

    local map complete

--- include/local_map/local_map.h
+++ include/local_map/local_map.h
@@ -14,7 +14,8 @@ public:
 private:
     void scan_callback(const sensor_msgs::LaserScan::ConstPtr &);
     void create_local_map();
-    void set_grid_map(const int& set_value);
+    void set_grid_map(const int& set_value,const float& radius);
+    int verify_index(const int&) ;
     int hz = 0;
     int set_value = 0;
     int world = 0;
@@ -23,12 +24,17 @@ private:
     int base_angle = 0;
     int max_index = 0;
     int min_index = 0;
+    int alliance = 0;
+    int index = 0;
     float roomba_radius = 0.3;//ルンバの半径 30cm
     float observation_radius = 0;
     float degree = 0;
-    float resolution = 0.05; //5cm
+    float resolution = 0.02; //2cm
+    float average = 0.0;
     float radius = 0;
     float theta = 0;
+    float max_radius = 0.0;
+    float estimate_radius = 0.0;
     float angle_min =0;
     float angle_max = 0;
     int angle_increment = 0;
--- src/local_map.cpp
+++ src/local_map.cpp
@@ -10,6 +10,7 @@ Local_map::Local_map():private_nh("~")
     private_nh.param("hz",hz,{1});
     private_nh.param("world",world,{5});
     private_nh.param("degree",degree,{5});
+    private_nh.param("alliance",alliance,{5});
     sub_scan = nh.subscribe("scan",10,&Local_map::scan_callback,this);
     pub_local_map = nh.advertise<nav_msgs::OccupancyGrid>("local_map",1);
 }
@@ -19,25 +20,60 @@ void Local_map::scan_callback(const sensor_msgs::LaserScan::ConstPtr &msg)
 
     laserscan = *msg;
     if(laserscan.ranges.size() != 0) create_local_map();
-    std::cout << "scan_callback"<<std::endl;
 }
 
-void Local_map::set_grid_map(const int& set_value)
+void Local_map::set_grid_map(const int& set_value,const float& radius)
 {
-    grid_map[row/2 + (int)(observation_radius*std::sin(theta)/resolution)][column/2 + (int)(observation_radius*std::cos(theta)/resolution)] = set_value;
-    for(float j=0 ;j<observation_radius; j+=resolution)
+    grid_map[row/2 + (int)(radius*std::sin(theta)/resolution)][column/2 + (int)(radius*std::cos(theta)/resolution)] = set_value;
+    for(float j=0 ;j<radius; j+=resolution)
     {
         /*if(j <= roomba_radius) grid_map[row/2 +(int)((j*std::sin(theta))/resolution)][column/2 + (int)((j*std::cos(theta))/resolution)] = -1;*/
-
         grid_map[row/2 +(int)((j*std::sin(theta))/resolution)][column/2 + (int)((j*std::cos(theta))/resolution)] = 0;
+    }
+    for(float x=world/2;  x>radius+0.1 ; x-=resolution)
+    {
+        grid_map[row/2 +(int)((x*std::sin(theta))/resolution)][column/2 + (int)((x*std::cos(theta))/resolution)] = -1;
+    }
+
+}
+int Local_map::verify_index(const int& i)
+{
+    if(i >= 348 && i <= 409)
+    {
+         average =  (348+409)/2;
+         if(average < i)
+         {
+            return 2;
+         }
+         else
+         {
+             return 3;
+         }
+    }
+    if(i >= 700 && i <= 762)
+    {
+        average = (700+762)/2;
+        if(average < i)
+        {
+            return 4;
+        }
+        else
+        {
+            return 5;
+        }
+    }
+    else
+    {
+        return 1;
     }
 }
 
 void Local_map::create_local_map()
 {
+
     row = (int)(world/resolution);
     column = (int)(world/resolution);
-
+    grid_map.clear();
     grid_map.resize(row,std::vector<int>(column,-1));
 
     angle_min = laserscan.angle_min;
@@ -46,22 +82,92 @@ void Local_map::create_local_map()
     base_angle = 540;
     max_index = (int)(((M_PI/2)/laserscan.angle_increment) + 540);
     min_index = (int)(((-M_PI/2)/laserscan.angle_increment) + 540);
-
-    for(int i=min_index; i<=max_index; i+=angle_increment)
+    for(int i=min_index; i<=max_index; i++)
     {
         observation_radius = laserscan.ranges[i];
-        theta = (i-540)*angle_increment;
+        theta = (i-540)*laserscan.angle_increment;
 
-        if(observation_radius > roomba_radius)
+        if(observation_radius < world/2)
         {
-            set_value = 100;
-            set_grid_map(set_value);
+            switch(verify_index(i))
+            {
+                case 1:
+                    set_value = 100;
+                    set_grid_map(set_value,observation_radius);
+                    break;
+                case 2:
+                    index = 348 -alliance;
+                    estimate_radius = laserscan.ranges[index];
+                    if(estimate_radius < world/2)
+                    {
+                        set_value = 100;
+                        set_grid_map(set_value,estimate_radius);
+                    }
+                    else
+                    {
+                        set_value = 100;
+                        set_grid_map(set_value,world/2);
+                    }
+                    break;
+
+                case 3:
+                    index = 409 + alliance;
+                    estimate_radius = laserscan.ranges[index];
+                    if(estimate_radius < world/2)
+                    {
+                        set_value = 100;
+                        set_grid_map(set_value,estimate_radius);
+                    }
+                    else
+                    {
+                        set_value = 100;
+                        set_grid_map(set_value,world/2);
+                    }
+
+                    break;
+
+                case 4:
+                    index = 700 - alliance;
+                    estimate_radius = laserscan.ranges[index];
+                    if(estimate_radius < world/2)
+                    {
+                        set_value = 100;
+                        set_grid_map(set_value,estimate_radius);
+                    }
+                    else
+                    {
+                        set_value = 100;
+                        set_grid_map(set_value,world/2);
+                    }
+                    break;
+
+                case 5:
+                    index =  762 + alliance;
+                    estimate_radius = laserscan.ranges[index];
+                    if(estimate_radius < world/2)
+                    {
+                        set_value = 100;
+                        set_grid_map(set_value,estimate_radius);
+                    }
+                    else
+                    {
+                        set_value = 100;
+                        set_grid_map(set_value,world/2);
+                    }
+                    break;
+
+
+                default:
+                    break;
+            }
         }
         else
         {
             set_value = 0;
-            set_grid_map(set_value);
+            max_radius = world/2;
+            set_grid_map(set_value,max_radius);
         }
+
     }
 
     nav_msgs::OccupancyGrid local_map;
@@ -88,7 +194,7 @@ void Local_map::process()
 
     while(ros::ok())
     {
-        create_local_map();
+        //create_local_map();
         ros::spinOnce();
         loop_rate.sleep();
     }
